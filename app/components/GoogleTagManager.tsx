87d6a1213b265dfbd137379d09530927edee82d2
heider-mostafa <mostafa.heider9@gmail.com>
2024-12-16 15:47:30 -0500

    Create GoogleTagManager.tsx

---
+++ app/components/GoogleTagManager.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useEffect } from 'react'
+import Script from 'next/script'
+
+export function GoogleTagManager() {
+  useEffect(() => {
+    const initGTM = () => {
+      (window as any).dataLayer = (window as any).dataLayer || [];
+      (window as any).dataLayer.push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+    }
+
+    initGTM();
+  }, []);
+
+  return (
+    <>
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N3KDKXZW');
+          `,
+        }}
+      />
+      <noscript>
+        <iframe 
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N3KDKXZW"
+          height="0" 
+          width="0" 
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+    </>
+  )
+}
+
