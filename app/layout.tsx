397ccca32bf111af5b257216a0e9f9cc048883ab
heider-mostafa <mostafa.heider9@gmail.com>
2024-12-16 15:48:23 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Header } from './components/Header'
-import Script from 'next/script'
+import { GoogleTagManager } from './components/GoogleTagManager'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,30 +21,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-N3KDKXZW');
-            `,
-          }}
-        />
-      </head>
       <body className={inter.className}>
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N3KDKXZW"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
+        <GoogleTagManager />
         <CartProvider>
           <Header />
           <main>{children}</main>
